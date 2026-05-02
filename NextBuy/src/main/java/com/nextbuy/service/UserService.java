af850550a22f4b0f26fbda43d81a553821ee43b2
ragolusrinu13-lang <ragolusrinu13@gmail.com>
2026-05-02 12:28:31 +0530

    passwordEncoder

--- NextBuy/src/main/java/com/nextbuy/service/UserService.java
+++ NextBuy/src/main/java/com/nextbuy/service/UserService.java
@@ -1,5 +1,22 @@
 package com.nextbuy.service;
 
+
+import org.springframework.stereotype.Service;
+
+import com.nextbuy.repository.UserRepository;
+
+@Service
 public class UserService {
+	
+	  UserRepository userRepo;
+
+	 public UserService(UserRepository userRepo) {
+		super();
+		this.userRepo = userRepo;
+	 }
+	 
+	 
+	 
+	
 
 }
