94b6726b094712aad1c0f4e25cf62670030e099a
Jaswanth778012 <kumarjaswanth101@gmail.com>
2026-05-02 12:54:38 +0530

    Jwt Service

--- NextBuy/src/main/java/com/nextbuy/repository/UserRepository.java
+++ NextBuy/src/main/java/com/nextbuy/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.nextbuy.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nextbuy.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	Optional<User> findByUsername(String username);
+	
+	boolean existsByUsername(String username);
 }
