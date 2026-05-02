d987d32613580be936c5d234f363c45b265c42a3
Jaswanth778012 <kumarjaswanth101@gmail.com>
2026-05-02 11:31:14 +0530

    User Operarion

---
+++ NextBuy/src/main/java/com/nextbuy/entity/User.java
@@ -0,0 +1,68 @@
+package com.nextbuy.entity;
+
+import java.time.LocalDate;
+
+import com.nextbuy.enums.Gender;
+import com.nextbuy.enums.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true, length = 50)
+	private String username;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private Long mobileNumber;
+	
+	@Column
+	private String email;
+	
+	@Enumerated(EnumType.STRING)
+	@Column
+	private Gender gender;
+	
+	@Enumerated(EnumType.STRING)
+	@Column
+	private Role role;
+	
+	@Column
+	private String addressLine1;
+	
+	@Column
+	private String dpUrl;
+	
+	@Column
+	private LocalDate dob;
+	
+	@Column
+	private LocalDate createdAt;
+	
+	@Column
+	private LocalDate lastLogin;
+}
