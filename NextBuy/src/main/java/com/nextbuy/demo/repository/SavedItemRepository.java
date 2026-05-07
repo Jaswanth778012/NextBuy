32d0dd1e6cbac10ab53750d26e3467141b8d1f83
Jaswanth778012 <kumarjaswanth101@gmail.com>
2026-05-07 15:56:07 +0530

    savedItem

--- NextBuy/src/main/java/com/nextbuy/demo/repository/SavedItemRepository.java
+++ NextBuy/src/main/java/com/nextbuy/demo/repository/SavedItemRepository.java
@@ -2,6 +2,8 @@ package com.nextbuy.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.nextbuy.demo.entity.SavedItem;
+
 public interface SavedItemRepository extends JpaRepository<SavedItem, Long> {
 
 }
